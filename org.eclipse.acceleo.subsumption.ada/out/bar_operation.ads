ff3d4ba8b679a31a6d0778d3607245fcc5aa1ae3
Piotr Trojanek <piotr.trojanek@gmail.com>
2011-05-15 22:54:11 +0200

    .gitignore cleanup

--- org.eclipse.acceleo.subsumption.ada/out/bar_operation.ads
+++
@@ -1,6 +0,0 @@
-with bar_buffers; use bar_buffers;
-
-package bar_operation is
-   -- This operation is executed periodically
-   procedure operation (inputs : in inputs_t);
-end bar_operation;
--- org.eclipse.acceleo.subsumption.ada/out/foo_operation.ads
+++
@@ -1,6 +0,0 @@
-with foo_buffers; use foo_buffers;
-
-package foo_operation is
-   -- This operation is executed periodically
-   function operation (inputs : in inputs_t) return outputs_t;
-end foo_operation;
